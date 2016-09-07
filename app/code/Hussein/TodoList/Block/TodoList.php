d1e321f08d9c7d21c506adbc7e162000a2e78c14
husseinwalli <husseinwalli@gmail.com>
2016-09-07 01:02:46 -0500

    Initial commit including todo list module structure

---
+++ app/code/Hussein/TodoList/Block/TodoList.php
@@ -0,0 +1,10 @@
+<?php 
+namespace Hussein\TodoList\Block;
+
+class TodoList extends \Magento\Framework\View\Element\Template
+{
+  public function _prepareLayout()
+  {
+    return parent::_prepareLayout();
+  }
+}
